2c31287a4da4e39cd8d029ebd48ea65ca04b9638
Kristian Schwienbacher <kr3dde@gmail.com>
2020-04-11 19:49:20 +0200

    Add gitlab ci file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # This file will be run in the docker container
 import os.path
-import sys 
+import sys
 
 # check if dataset has been downloaded
 if not os.path.isdir(os.curdir + '/data/dev_dataset/CheXpert-v1.0-small'):
