e7603f150557ea1e7b5f12d5413bd1d30d819cca
Johanna Sommer <johanna@mail-sommer.com>
2020-04-12 13:08:46 +0200

    closes #16 scaling with scikit image

--- src/preprocessing/rescaling/Scaler.py
+++ src/preprocessing/rescaling/Scaler.py
@@ -1,19 +1,35 @@
 from skimage.transform import resize
 import cv2
 import matplotlib.image as mpimg
+from deprecated import deprecated
 
 
 class Scaler:
-    def __init__(self, path, outsize):
-        self.path = path
+    def __init__(self, img, outsize):
+        self.img = img
         self.outsize = (outsize, outsize)
 
-    def skimg_resize(self):
-        img = mpimg.imread(self.path)
-        return resize(img, self.outsize)
+    def resize(self, interpolation):
+        """
+        resize input image with skikit image functionality
+        parameters:
+        interpolation -- type of interpolation used for resizing
+        """
+        if interpolation == 'NEAREST':
+            out = resize(image=self.img, output_shape=self.outsize, order=0)
+        elif interpolation == 'LINEAR':
+            out = resize(image=self.img, output_shape=self.outsize, order=1)
+        elif interpolation == 'QUADRATIC':
+            out = resize(image=self.img, output_shape=self.outsize, order=2)
+        elif interpolation == 'CUBIC':
+            out = resize(image=self.img, output_shape=self.outsize, order=3)
+        else:
+            raise NotImplementedError()
+        return out
 
-    def bilinear_interpolate(self, interpolation):
-        img = cv2.imread(self.path)
+    @deprecated(version='1.0.0', reason="Not as efficient in np.array handling, use scikit image implementation instead")
+    def cv2_interpolate(self, interpolation, path):
+        img = cv2.imread(path)
         if interpolation == 'NEAREST':
             out = cv2.resize(img, self.outsize, interpolation=cv2.INTER_NEAREST)
         elif interpolation == 'LINEAR':
