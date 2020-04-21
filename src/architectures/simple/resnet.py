5baf515019f1269bbb75dccf7ad35c30c106cb49
Johanna Sommer <johanna@mail-sommer.com>
2020-04-21 18:50:05 +0200

    added pre-implemented resnet model

---
+++ src/architectures/simple/resnet.py
@@ -0,0 +1,55 @@
+import os
+import keras
+import cv2
+import pandas as pd
+import numpy as np
+from skimage.transform import resize
+
+
+def make_dataset(data, rgb):
+    print('reading dataset')
+    x_data, y_data = [], []
+    for ind in data.index:
+        target = data['Cardiomegaly'][ind]
+        if not pd.isna(target):
+            img = cv2.imread(os.path.join(dataset_folder + data['Path'][ind]), cv2.IMREAD_GRAYSCALE)
+            scaled = resize(image=img, output_shape=(224, 224), order=1)
+            if rgb:
+                x_data.append(np.stack((scaled, scaled, scaled), axis=2))
+            else:
+                x_data.append(scaled)
+            if target == -1:
+                y_data.append(np.uint8(2))
+            else:
+                y_data.append(np.uint8(target))
+    return np.array(x_data), np.array(y_data)
+
+
+dataset_folder = "../../../data/dataset/"
+chexpert_folder = dataset_folder + "CheXpert-v1.0-small/"
+
+model = keras.applications.resnet.ResNet152(include_top=True,
+                                                weights=None, pooling=None, classes=3)
+
+model.compile(optimizer=keras.optimizers.RMSprop(),
+              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['sparse_categorical_accuracy'])
+
+data_train = pd.read_csv(os.path.join(dataset_folder + 'train.csv'))
+data_train = data_train[:80000]
+x_train, y_train = make_dataset(data_train, rgb=True)
+data_valid = pd.read_csv(os.path.join(dataset_folder + 'valid.csv'))
+x_valid, y_valid = make_dataset(data_valid, rgb=True)
+
+print('# Fit model on training data')
+history = model.fit(x_train, y_train,
+                    batch_size=16,
+                    epochs=10,
+                    validation_data=(x_valid, y_valid))
+
+print('\nhistory dict:', history.history)
+# model.save('densenet.h5')
+
+# predictions = model.predict(x_test[:10])
+# print(predictions)
+# print(y_test[:10])
