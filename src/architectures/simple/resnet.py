04ae68345509434fbdfb6d1d086f2c1c078da07c
Tobias Schmidt <tobias.schmidt@atos.net>
2020-04-23 14:33:09 +0200

    WIP resnet 3

--- src/architectures/simple/resnet.py
+++ src/architectures/simple/resnet.py
@@ -1,13 +1,9 @@
 import os
-import keras
-import cv2
 import pandas as pd
 import numpy as np
-from skimage.transform import resize
-from pathlib import Path
+import keras
 from keras_preprocessing.image import ImageDataGenerator
-from  keras.applications import resnet_v2
-from  keras.applications import inception_v3
+from keras.applications import resnet_v2
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.models import Model
 from sklearn.model_selection import train_test_split
@@ -31,32 +27,50 @@ data = data[~data['Frontal/Lateral'].str.contains("Lateral")]
 data = data.drop(["Sex", "Age", "Frontal/Lateral", "AP/PA"], axis=1)
 
 # deal with uncertanty (-1) values
-data = data.replace(-1,1)
+data = data.replace(-1, 1)
 
 np.random.seed(SEED)
 data_train, data_test = train_test_split(data, test_size=0.2)
 data_train, data_val = train_test_split(data_train, test_size=0.2)
 
 
-train_datagen=ImageDataGenerator(rescale=1./255)
-valid_datagen=ImageDataGenerator(rescale=1./255.)
-test_datagen=ImageDataGenerator(rescale=1./255.)
+train_datagen = ImageDataGenerator(rescale=1./255)
+valid_datagen = ImageDataGenerator(rescale=1./255.)
+test_datagen = ImageDataGenerator(rescale=1./255.)
 
 
-target_size = (224,224)
-train_generator=train_datagen.flow_from_dataframe(
-    dataframe=data_train, directory=DATASET_FOLDER , x_col='Path', y_col=list(data_train.columns[2:16]), class_mode='other', target_size=target_size, batch_size=32
+target_size = (224, 224)
+train_generator = train_datagen.flow_from_dataframe(
+    dataframe=data_train,
+    directory=DATASET_FOLDER,
+    x_col='Path',
+    y_col=list(data_train.columns[2:16]),
+    class_mode='other',
+    target_size=target_size,
+    batch_size=32
 )
 valid_generator = valid_datagen.flow_from_dataframe(
-    dataframe=data_val, directory=DATASET_FOLDER , x_col='Path', y_col=list(data_val.columns[2:16]), class_mode='other', target_size=target_size, batch_size=32
+    dataframe=data_val,
+    directory=DATASET_FOLDER,
+    x_col='Path',
+    y_col=list(data_val.columns[2:16]),
+    class_mode='other',
+    target_size=target_size,
+    batch_size=32
 )
 test_generator = test_datagen.flow_from_dataframe(
-    dataframe=data_test, directory=DATASET_FOLDER , x_col="Path", y_col=list(data_test.columns[2:16]), class_mode="other", target_size=target_size, shuffle=False, batch_size=1
+    dataframe=data_test,
+    directory=DATASET_FOLDER,
+    x_col="Path",
+    y_col=list(data_test.columns[2:16]),
+    class_mode="other",
+    target_size=target_size,
+    shuffle=False, batch_size=1
 )
 
 base_model = resnet_v2.ResNet152V2(include_top=False, weights='imagenet')
 
-# add global pooling and dense output layer 
+# add global pooling and dense output layer
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
 x = Dense(1024, activation='relu')(x)
@@ -67,23 +81,23 @@ model = Model(inputs=base_model.input, outputs=prediction_layer)
 # freeze all convolutional layers
 for layer in base_model.layers:
     layer.trainable = False
-    
+
 
 # compile model
 adam = keras.optimizers.Adam()
 model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
 
 
-# fit model 
+# fit model
 num_epochs = 3
-STEP_SIZE_TRAIN= train_generator.n // train_generator.batch_size
-STEP_SIZE_VALID= valid_generator.n // valid_generator.batch_size
+STEP_SIZE_TRAIN = train_generator.n // train_generator.batch_size
+STEP_SIZE_VALID = valid_generator.n // valid_generator.batch_size
 
 result = model.fit_generator(generator=train_generator,
-                    steps_per_epoch=STEP_SIZE_TRAIN,
-                    validation_data=valid_generator,
-                    validation_steps=STEP_SIZE_VALID,
-                    epochs=num_epochs)
+                             steps_per_epoch=STEP_SIZE_TRAIN,
+                             validation_data=valid_generator,
+                             validation_steps=STEP_SIZE_VALID,
+                             epochs=num_epochs)
 
 # save the model
 model.save('models/resnet/resnet50-v2.h5')
