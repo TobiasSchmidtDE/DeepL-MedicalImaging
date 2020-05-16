53b79022235a5077ebf13d70e76f5b94f3257720
Kristian Schwienbacher <kristian.schwienbacher@tum.de>
2020-05-16 12:57:07 +0200

    testing changes WIP

--- remote_access/open_remoteaccess.sh
+++ remote_access/open_remoteaccess.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-mkdir -p /srv/idp-radio-1/nohup/
-/srv/idp-radio-1/remote_access/ngrok start -config tunnel.cfg --all > /dev/null &
-nohup jupyter lab --ip=0.0.0.0 --port=8888 --NotebookApp.allow_password_change=False --NotebookApp.password="sha1:bbea9cdc2680:5b7c455593d9ed710a81c0fa4e5af3e91e941237" --ContentsManager.allow_hidden=True --allow-root --no-browser > /srv/idp-radio-1/nohup/nohup_jupyterlab.out &
-nohup jupyter notebook --ip=0.0.0.0 --port=8080 --NotebookApp.allow_password_change=False  --NotebookApp.password="sha1:bbea9cdc2680:5b7c455593d9ed710a81c0fa4e5af3e91e941237" --allow-root --no-browser > /srv/idp-radio-1/nohup/nohup_jupyternotebook.out &
-nohup tensorboard --logdir /srv/idp-radio-1/models/ --host 0.0.0.0 --port 6006 > /srv/idp-radio-1/nohup/nohup_tensorboard.out &
+
+PASS_HASH="sha1:bbea9cdc2680:5b7c455593d9ed710a81c0fa4e5af3e91e941237"
+HOST_IP=0.0.0.0
+ROOT_DIR=/srv/idp-radio-1
+LOG_DIR=$ROOT_DIR/nohup/
+
+echo $LOG_DIR
+
+mkdir -p $LOG_DIR
+$ROOT_DIR$ROOT_DIR/remote_access/ngrok start -config tunnel.cfg --all > /dev/null &
+nohup jupyter lab --ip=$HOST_IP --port=8888 --NotebookApp.allow_password_change=False --NotebookApp.password=$PASS_HASH --ContentsManager.allow_hidden=True --allow-root --no-browser > $LOG_DIR/nohup_jupyterlab.out &
+nohup jupyter notebook --ip=$HOST_IP --port=8080 --NotebookApp.allow_password_change=False  --NotebookApp.password=$PASS_HASH --allow-root --no-browser > $LOG_DIR/nohup_jupyternotebook.out &
+nohup tensorboard --logdir $ROOT_DIR/models/ --host $HOST_IP --port 6006 > $LOG_DIR/nohup_tensorboard.out &
 echo
-/srv/idp-radio-1/remote_access/get_tunnels.sh
+
+$ROOT_DIR/remote_access/get_tunnels.sh
