c53c6c421872658082bcde765f64f782f9ea6ae6
Kristian Schwienbacher <kr3dde@gmail.com>
2020-05-20 15:40:27 +0200

    Add train test split function

---
+++ src/preprocessing/split/train_test_split.py
@@ -0,0 +1,33 @@
+from sklearn.model_selection import GroupShuffleSplit
+
+
+def train_test_split(data, test_size=0.2, seed=None):
+    """
+     Split dataset into random train and test subsets, while keeping all the
+     samples of one patient in the same set.
+
+     Parameters:
+        data (pd.Dataframe):
+          The data that should be splitted
+        test_size (float):
+          A number between 0.0 and 1.0 specifying the size of the test set.
+        seed (int):
+          Controlls the shuffling applied to the data before the it is split.
+     Returns:
+        splits (list):
+          A list consisting of the train and test split
+    """
+
+    # create the group shuffle splitter
+    shuffle_split = GroupShuffleSplit(
+        test_size=test_size, random_state=seed)
+
+    # define groups as patient ids
+    groups = data['patient_id'].to_numpy()
+
+    train_idx, test_idx = next(shuffle_split.split(data, groups=groups))
+
+    train_data = data.iloc[train_idx]
+    test_data = data.iloc[test_idx]
+
+    return [train_data, test_data]
