2edc235048f66738225f2219f3f0ea23e7087684
Johanna Sommer <johanna@mail-sommer.com>
2020-05-20 11:40:38 +0200

    added model compilation

--- src/architectures/simple/simple_base.py
+++ src/architectures/simple/simple_base.py
@@ -3,28 +3,45 @@ from keras.applications.inception_v3 import InceptionV3
 from keras.applications.densenet import DenseNet121
 from keras.applications.resnet_v2 import ResNet152V2
 from src.datasets.generator import ImageDataGenerator
+from tensorflow.keras.models import Model
+from keras.optimizers import Adam
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 
 
 class SimpleBase:
-    def __init__(self, model_name, dataset_folder, columns):
+    def __init__(self, model_name, dataset_folder, columns,
+                 optimizer=Adam(), loss='binary_crossentropy', metrics=None):
+        if metrics is None:
+            metrics = ['accuracy']
+
         if model_name == 'inceptionv3':
-            self.model = InceptionV3(include_top=False, weights='imagenet')
+            base_model = InceptionV3(include_top=False, weights='imagenet')
         elif model_name == 'densenet121':
-            self.model = DenseNet121(include_top=False, weights='imagenet')
+            base_model = DenseNet121(include_top=False, weights='imagenet')
         elif model_name == 'resnet152v2':
-            self.model = ResNet152V2(include_top=False, weights='imagenet')
+            base_model = ResNet152V2(include_top=False, weights='imagenet')
         # TODO: implement ResNext, other densenet
         else:
             raise NotImplementedError()
+
+        x = base_model.output
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(1024, activation='relu')(x)
+        prediction_layer = Dense(len(columns), activation='sigmoid')(x)
+        self.model = Model(inputs=base_model.input, outputs=prediction_layer)
+        self.model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 
         self.dataset_folder = dataset_folder
         train_dataset = pd.read_csv(self.dataset_folder / 'train.csv', index_col=[0])
         val_dataset = pd.read_csv(self.dataset_folder / 'val.csv', index_col=[0])
         test_dataset = pd.read_csv(self.dataset_folder / 'test.csv', index_col=[0])
 
-        self.traingen = ImageDataGenerator(dataset=train_dataset,dataset_folder=self.dataset_folder, label_columns=columns)
-        self.valgen = ImageDataGenerator(dataset=val_dataset,dataset_folder=self.dataset_folder, label_columns=columns)
-        self.testgen = ImageDataGenerator(dataset=test_dataset,dataset_folder=self.dataset_folder, label_columns=columns)
+        self.traingen = ImageDataGenerator(dataset=train_dataset, dataset_folder=self.dataset_folder,
+                                           label_columns=columns)
+        self.valgen = ImageDataGenerator(dataset=val_dataset, dataset_folder=self.dataset_folder,
+                                         label_columns=columns)
+        self.testgen = ImageDataGenerator(dataset=test_dataset, dataset_folder=self.dataset_folder,
+                                          label_columns=columns)
 
     def train_model(self):
         print('todo')
