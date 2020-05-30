34cd8a46106ee5c230f2c74e8ca2006ea3f59da8
Kristian Schwienbacher <kr3dde@gmail.com>
2020-05-30 21:28:13 +0200

    Fix lint error

--- src/architectures/adv/guendel19.py
+++ src/architectures/adv/guendel19.py
@@ -1,6 +1,4 @@
-from keras import layers
-from keras import backend
-from keras import models
+import tensorflow as tf
 
 
 def dense_block(x, blocks, name):
@@ -28,14 +26,14 @@ def transition_block(x, reduction, name):
         # Returns
             output tensor for the block
     """
-    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
-    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
-                                  name=name + '_bn')(x)
-    x = layers.Activation('relu', name=name + '_relu')(x)
-    x = layers.Conv2D(int(backend.int_shape(x)[bn_axis] * reduction), 1,
-                      use_bias=False,
-                      name=name + '_conv')(x)
-    x = layers.AveragePooling2D(2, strides=2, name=name + '_pool')(x)
+    bn_axis = 3 if tf.keras.backend.image_data_format() == 'channels_last' else 1
+    x = tf.keras.layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
+                                           name=name + '_bn')(x)
+    x = tf.keras.layers.Activation('relu', name=name + '_relu')(x)
+    x = tf.keras.layers.Conv2D(int(tf.keras.backend.int_shape(x)[bn_axis] * reduction), 1,
+                               use_bias=False,
+                               name=name + '_conv')(x)
+    x = tf.keras.layers.AveragePooling2D(2, strides=2, name=name + '_pool')(x)
     return x
 
 
@@ -48,22 +46,23 @@ def conv_block(x, growth_rate, name):
        Returns
            Output tensor for the block
     """
-    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
-    x1 = layers.BatchNormalization(axis=bn_axis,
-                                   epsilon=1.001e-5,
-                                   name=name + '_0_bn')(x)
-    x1 = layers.Activation('relu', name=name + '_0_relu')(x1)
-    x1 = layers.Conv2D(4 * growth_rate, 1,
-                       use_bias=False,
-                       name=name + '_1_conv')(x1)
-    x1 = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
-                                   name=name + '_1_bn')(x1)
-    x1 = layers.Activation('relu', name=name + '_1_relu')(x1)
-    x1 = layers.Conv2D(growth_rate, 3,
-                       padding='same',
-                       use_bias=False,
-                       name=name + '_2_conv')(x1)
-    x = layers.Concatenate(axis=bn_axis, name=name + '_concat')([x, x1])
+    bn_axis = 3 if tf.keras.backend.image_data_format() == 'channels_last' else 1
+    x1 = tf.keras.layers.BatchNormalization(axis=bn_axis,
+                                            epsilon=1.001e-5,
+                                            name=name + '_0_bn')(x)
+    x1 = tf.keras.layers.Activation('relu', name=name + '_0_relu')(x1)
+    x1 = tf.keras.layers.Conv2D(4 * growth_rate, 1,
+                                use_bias=False,
+                                name=name + '_1_conv')(x1)
+    x1 = tf.keras.layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
+                                            name=name + '_1_bn')(x1)
+    x1 = tf.keras.layers.Activation('relu', name=name + '_1_relu')(x1)
+    x1 = tf.keras.layers.Conv2D(growth_rate, 3,
+                                padding='same',
+                                use_bias=False,
+                                name=name + '_2_conv')(x1)
+    x = tf.keras.layers.Concatenate(
+        axis=bn_axis, name=name + '_concat')([x, x1])
     return x
 
 
@@ -79,16 +78,17 @@ def densenet(classes=14):
             densenet architecture model
     """
     blocks = [6, 12, 24, 16]
-    img_input = layers.Input(shape=(256, 256, 3))
-    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
+    img_input = tf.keras.layers.Input(shape=(256, 256, 3))
+    bn_axis = 3 if tf.keras.backend.image_data_format() == 'channels_last' else 1
 
-    x = layers.ZeroPadding2D(padding=((3, 3), (3, 3)))(img_input)
-    x = layers.Conv2D(64, 7, strides=2, use_bias=False, name='conv1/conv')(x)
-    x = layers.BatchNormalization(
+    x = tf.keras.layers.ZeroPadding2D(padding=((3, 3), (3, 3)))(img_input)
+    x = tf.keras.layers.Conv2D(
+        64, 7, strides=2, use_bias=False, name='conv1/conv')(x)
+    x = tf.keras.layers.BatchNormalization(
         axis=bn_axis, epsilon=1.001e-5, name='conv1/bn')(x)
-    x = layers.Activation('relu', name='conv1/relu')(x)
-    x = layers.ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
-    x = layers.MaxPooling2D(3, strides=2, name='pool1')(x)
+    x = tf.keras.layers.Activation('relu', name='conv1/relu')(x)
+    x = tf.keras.layers.ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
+    x = tf.keras.layers.MaxPooling2D(3, strides=2, name='pool1')(x)
 
     x = dense_block(x, blocks[0], name='conv2')
     x = transition_block(x, 0.5, name='pool2')
@@ -98,13 +98,13 @@ def densenet(classes=14):
     x = transition_block(x, 0.5, name='pool4')
     x = dense_block(x, blocks[3], name='conv5')
 
-    x = layers.BatchNormalization(
+    x = tf.keras.layers.BatchNormalization(
         axis=bn_axis, epsilon=1.001e-5, name='bn')(x)
-    x = layers.Activation('relu', name='relu')(x)
+    x = tf.keras.layers.Activation('relu', name='relu')(x)
 
-    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
-    x = layers.Dense(classes, activation='sigmoid', name='fc')(x)
+    x = tf.keras.layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    x = tf.keras.layers.Dense(classes, activation='sigmoid', name='fc')(x)
 
-    model = models.Model(img_input, x, name='densenet')
+    model = tf.keras.models.Model(img_input, x, name='densenet')
 
     return model
