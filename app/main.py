e4511e58de9f7fffaf80bb867f95d99cb63b938a
Kristian Schwienbacher <kr3dde@gmail.com>
2020-08-29 11:01:53 +0200

    Add streamlit application

--- app/main.py
+++ app/main.py
@@ -7,6 +7,11 @@ from src.utils.crm import CRM, decode_predictions
 import io
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
+import numpy as np
+
+colors = ['#F79F1F', '#A3CB38', '#1289A7',
+          '#D980FA', '#B53471', '#EE5A24', '#009432', '#0652DD', '#9980FA', '#EA2027', '#5758BB', '#ED4C67']
+
 
 basepath = Path(os.getcwd())
 # make sure your working directory is the repository root.
@@ -22,60 +27,77 @@ models = ['DenseNet121_Chexpert_CWBCE_L1Normed_E3_B32_C0_N12_AugAffineColor_shar
 
 model_name = st.selectbox('Select the model', models)
 
-
-crm = build_crm(model_name)
+with st.spinner('Loading model....'):
+    crm = build_crm(model_name)
 
 image = st.file_uploader("Upload image")
 
-thresh = st.slider('Threshold', 0.0, 1.0, 0.3)
+thresh = st.slider('Threshold for bounding boxes', 0.0, 1.0, 0.3)
 
-if image:
-    with open("app/temp.png", "wb") as f:
-        f.write(image.getbuffer())
+with st.spinner('Evaluating image....'):
+    if image:
+        with open("app/temp.png", "wb") as f:
+            f.write(image.getbuffer())
 
-    original, resized_crm, img, output = crm.single_image_crm(
-        'app/temp.png', thresh)
+        original, resized_crm, img, output = crm.single_image_crm(
+            'app/temp.png', thresh)
 
-    visualization = st.selectbox('Select the visualization mode', [
-        'combined', 'class based'])
+        visualization = st.selectbox('Select the visualization mode', [
+            'combined', 'class based'])
 
-    top = decode_predictions(crm.classes, output[0], crm.num_classes)[:7]
+        top = decode_predictions(crm.classes, output[0], crm.num_classes)[:7]
 
-    bbox, plt = crm.plot_crm(original, img, resized_crm, thresh)
-
-    if visualization == 'combined':
-        for c, i, p in top:
-            st.write('{:15s}({}) {:f}'.format(c, i, p))
-        bbox, plt = crm.plot_crm(original, img, resized_crm, thresh)
-
-        st.subheader("Combined CRM Plot")
-        st.write(plt)
-
-    if visualization == 'class based':
-        st.subheader("Class based CRM Plot")
-        boxes = []
-        for c, i, p in top[:3]:
-            if p > 0.3:
-                original, resized_crm, img, output = crm.single_image_crm(
-                    'app/temp.png', thresh, class_idx=i)
+        if visualization == 'combined':
+            for c, i, p in top:
                 st.write('{:15s}({}) {:f}'.format(c, i, p))
-                bbox, fig = crm.plot_crm(original, img, resized_crm, thresh)
-                st.write(fig)
-                boxes = boxes + bbox.tolist()
+            bbox, plot = crm.plot_crm(original, img, resized_crm, thresh)
 
-        fig = plt.figure(figsize=(15, 10))
-        ax = plt.subplot(111)
-        plt.imshow(original)
+            st.subheader("Combined CRM Plot")
+            st.write(plot)
 
-        for bbox in boxes:
-            xs = bbox[1]
-            ys = bbox[0]
-            w = bbox[3] - bbox[1]
-            h = bbox[2] - bbox[0]
-            rect = patches.Rectangle((ys, xs), w, h, linewidth=1,
-                                     edgecolor='r', facecolor='none')
+        if visualization == 'class based':
+            st.subheader("Class based CRM Plot")
+            pred_thresh = st.slider('Prediction threshold', 0.0, 1.0, 0.1)
 
-            # Add the patch to the Axes
-            ax.add_patch(rect)
+            boxes = []
+            for c, i, p in top[:3]:
+                if p > pred_thresh:
+                    original, resized_crm, img, output = crm.single_image_crm(
+                        'app/temp.png', thresh, class_idx=i)
+                    st.write('{:15s}({}) {:f}'.format(c, i, p))
+                    bbox, fig = crm.plot_crm(
+                        original, img, resized_crm, thresh)
+                    st.write(fig)
+                    newboxes = bbox.tolist()
+                    newboxes = [(i, box) for box in newboxes]
+                    boxes = boxes + newboxes
 
-        st.write(fig)
+            fig = plt.figure(figsize=(6, 4))
+            ax = plt.subplot(111)
+            plt.imshow(np.zeros((256, 256)))
+            plt.imshow(original)
+            plt.axis('off')
+
+            for box in boxes:
+                i = box[0]
+                bbox = box[1]
+                ys = bbox[1]
+                xs = bbox[0]
+                w = bbox[2] - bbox[0]
+                h = bbox[3] - bbox[1]
+
+                if w > 50 and h > 50:
+                    rect = patches.Rectangle((xs, ys), w, h, linewidth=1,
+                                             edgecolor=colors[i], facecolor='none')
+
+                    # Add the patch to the Axes
+                    ax.add_patch(rect)
+                    ax.annotate(crm.classes[i], (xs, ys), color='white',
+                                fontsize=6, ha='left', va='bottom')
+                    for c, j, p in top[:3]:
+                        if i == j:
+                            prob = p
+                    ax.annotate("{:.2f}".format(prob), (xs + w, ys), color='white',
+                                fontsize=6, ha='right', va='bottom')
+
+            st.write(fig)
